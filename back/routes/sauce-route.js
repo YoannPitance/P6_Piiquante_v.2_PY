5d99866fd92c73559b060b831a40c789759be1d8
YoannPitance <pittyo88@gmail.com>
2023-02-15 11:09:43 +0100

    like/dislike fonctionne sur une sauce mais pas sur les autres???

--- back/routes/sauce-route.js
+++ back/routes/sauce-route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const multer = require("../middlewares/multer-config");
 
 const sauceCtrl = require("../controllers/sauce-ctrl");
-//const likeCtrl = require("../controllers/likes");
+const likeCtrl = require("../controllers/likes");
 const auth = require("../middlewares/auth");
 
 router.get("/", auth, sauceCtrl.getAllSauce);
@@ -11,6 +11,6 @@ router.get("/:id", auth, sauceCtrl.getOneSauce);
 router.post("/", auth, multer, sauceCtrl.createSauce);
 router.put("/:id", auth, multer, sauceCtrl.modifySauce);
 router.delete("/:id", auth, sauceCtrl.deleteSauce);
-router.post("/:id/like", auth, sauceCtrl.likeSauce);
+router.post("/:id/like", auth, likeCtrl.likeSauce);
 
 module.exports = router;
