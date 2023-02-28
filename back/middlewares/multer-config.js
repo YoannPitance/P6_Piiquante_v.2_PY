52888a84bfe70357a281950915f3a1c821d949ed
YoannPitance <pittyo88@gmail.com>
2023-02-28 09:08:51 +0100

    petite modif de derniere minute sur le multer

--- back/middlewares/multer-config.js
+++ back/middlewares/multer-config.js
@@ -1,9 +1,9 @@
 const multer = require("multer");
 
 const MIME_TYPES = {
-  "images/jpg": "jpg",
-  "images/jpeg": "jpg",
-  "images/png": "png",
+  "image/jpg": "jpg",
+  "image/jpeg": "jpg",
+  "image/png": "png",
 };
 
 const storage = multer.diskStorage({
